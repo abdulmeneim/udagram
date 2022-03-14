bda89c260f1ae9c728775ce474cc0c01c3f6830b
abdulmeneim <ahmed.abd.meneim@gmail.com>
2022-03-15 00:10:07 +0200

    Update deploy.sh

--- udagram-api/deploy.sh
+++ udagram-api/deploy.sh
@@ -1,3 +1,16 @@
 eb use Project3api-env
+eb setenv POSTGRES_USERNAME=$POSTGRES_USERNAME
+eb setenv POSTGRES_PASSWOR=$POSTGRES_PASSWOR
+eb setenv POSTGRES_DB=$POSTGRES_DB
+eb setenv PORT=$PORT
+eb setenv PORT_project=$PORT_project
+eb setenv POSTGRES_HOST=$POSTGRES_HOST
+eb setenv AWS_REGION=$AWS_REGION
+eb setenv AWS_BUCKET=$AWS_BUCKET
+eb setenv AWS_PROFILE=$AWS_PROFILE
+eb setenv URL=$URL
+eb setenv JWT_SECRET=$JWT_SECRET
+
 eb deploy 
 eb health
+
