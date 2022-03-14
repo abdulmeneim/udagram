3a89358fefa9bc993989f1667dcc066f4cac5d1e
abdulmeneim <ahmed.abd.meneim@gmail.com>
2022-03-14 14:15:48 +0200

    up

--- udagram-frontend/e2e/src/app.e2e-spec.ts
+++ udagram-frontend/e2e/src/app.e2e-spec.ts
@@ -11,8 +11,8 @@ describe('new App', () => {
       page.navigateTo('/home');
     });
     it('should have a title saying Home', () => {
-      page.getPageOneTitleText().then(title => {
-        expect(title).toEqual('Home');
+      page.getTitle().then(title => {
+        expect(title).toEqual("Udagram");
       });
     });
   });
